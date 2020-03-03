017ca8e2deb73448bff9785061f63bb53c1f656b
PeterMerkx <48492623+PeterMerkx@users.noreply.github.com>
2020-03-03 15:21:42 +0100

    WorkingState without navigation
    WorkingState without navigation

--- MyBasicApplication/ViewModels/MainWindowViewModel.cs
+++ MyBasicApplication/ViewModels/MainWindowViewModel.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using MyBasicApplication.Core;
+using MyBasicApplication.Library;
 using MyBasicApplication.Views;
 using Prism.Commands;
 using System.Windows.Input;
